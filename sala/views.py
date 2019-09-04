aa157c7e2300d6a20fbe3f7d403712dc753dacc5
danilowOENS <dl539994@gmail.com>
2019-09-04 17:02:02 -0300

    Aprendendo-banco-de-dados

--- sala/views.py
+++ sala/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from sala.models import Aluno
+from sala.serializers import AlunoSerializer
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
 
 
 # Create your views here.
-
 # def cadastrar(request):
 
 #     if request.method == 'POST':
