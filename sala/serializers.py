0614517813cbcb3e4da1cedd6b837e5bb5296890
danilowOENS <dl539994@gmail.com>
2019-09-10 15:26:20 -0300

    projeto-sala-mudou-a-serializers

--- sala/serializers.py
+++ sala/serializers.py
@@ -1,27 +1,31 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from sala.models import Aluno
 from sala.models import Professor
 
+class ProfessorDataSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    nome = serializers.CharField(read_only=True)
 
 class AlunoSerializer(serializers.Serializer):
     id = serializers.ImageField(read_only=True)
     nome = serializers.CharField(max_length=255)
     idade = serializers.CharField()
 
-    prof_favorito = serializers.SlugRelatedField(
-        slug_field='nome',
-        queryset=Professor.objects.all()
-    )
+    prof_favorito = ProfessorDataSerializer()
 
 
     def create(self, validated_data):
-        aluno =Aluno.objects.create(**validated_data)
+        professor_data = validated_data.pop('prof_favorito')
+        professor = Professor.objects.get(id=professor_data['nome'])
+        aluno =Aluno.objects.create(prof_favorito=professor, **validated_data)
         return aluno
 
     def update(self, instance, validated_data):
         instance.nome = validated_data.get('nome')
         instance.idade = validated_data.get('idade')
         instance.email = validated_data.get('email')
-        instance.prof_favorito = validated_data.get('prof_favorito')
+        professor_data = validated_data.pop('prof_favorito')
+        professor = Professor.objects.get(id=professor_data['nome'])
+        instance.prof_favorito = professor
         instance.save()
         return instance
